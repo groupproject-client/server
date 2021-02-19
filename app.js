de4891235410120effdd3a2c0e31a29d3379d180
amgeraldo14 <amgeraldo14@gmail.com>
2021-02-19 11:07:10 +0700

    test deploy lagi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -127,6 +128,6 @@ io.on('connection', (socket) => {
   })
 })
 
-http.listen(3000, () => {
-  console.log('listening on *:3000')
+http.listen(PORT, () => {
+  console.log('listening on ' + PORT)
 })
